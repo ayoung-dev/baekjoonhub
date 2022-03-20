9c7b3d356f2647bf4d435148c3269d64fa15e282
AYOUNG YANG <52439725+a-young0210@users.noreply.github.com>
2022-03-20 21:09:28 +0900

    [Bronze V] Title: A+B, Time: 204 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,22 @@
+//scanner import
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        //scanner 선언
+        Scanner scanner = new Scanner(System.in);
+        
+        //첫 번째 숫자 입력 받기
+        int num1 = scanner.nextInt();
+        
+        //두 번째 숫자 입력 받기
+        int num2 = scanner.nextInt();
+        
+        //계산하기
+        int result = num1 + num2;
+        
+        //결과 출력
+        System.out.println(result);
+
+    }
+}
