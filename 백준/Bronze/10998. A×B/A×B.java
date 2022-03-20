bb8a70f3c32972eeebe4b31d920055ab4d9b1d84
AYOUNG YANG <52439725+a-young0210@users.noreply.github.com>
2022-03-20 21:23:53 +0900

    [Bronze V] Title: A×B, Time: 220 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,21 @@
+//scanner import
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        //scanner 선언
+        Scanner scanner = new Scanner(System.in);
+        
+        //첫 번재 숫자 입력 받기
+        int num1 = scanner.nextInt();
+        
+        //두 번째 숫자 입력 받기
+        int num2 = scanner.nextInt();
+        
+        //결과 출력
+        System.out.println(num1 * num2);
+        
+        //scanner 닫기
+        scanner.close();
+    }
+}
