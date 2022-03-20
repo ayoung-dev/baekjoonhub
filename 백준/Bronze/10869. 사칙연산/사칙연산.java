a16b159339523ed82248ccc2efad4a0a3142cdb3
AYOUNG YANG <52439725+a-young0210@users.noreply.github.com>
2022-03-20 21:41:40 +0900

    [Bronze V] Title: 사칙연산, Time: 220 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,34 @@
+//scanner import
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+    
+        //scanner 선언
+        Scanner scanner = new Scanner(System.in);
+        
+        //첫 번째 숫자 입력 받기
+		int num1 = scanner.nextInt();
+        
+        //두 번째 숫자 입력 받기
+		int num2 = scanner.nextInt();
+        
+		//더하기
+		System.out.println(num1 + num2);
+        
+        //빼기
+        System.out.println(num1 - num2);
+        
+        //곱하기
+        System.out.println(num1 * num2);
+        
+        //나누기 - 몫
+        System.out.println(num1 / num2);
+        
+        //나누기 - 나머지
+        System.out.println(num1 % num2);
+ 
+        //scanner 닫기
+		scanner.close();
+    }
+}
