65761cca36b3f3169c650b2b3fb529c0929709a8
AYOUNG YANG <52439725+a-young0210@users.noreply.github.com>
2022-03-22 19:15:30 +0900

    [Bronze III] Title: A+B - 3, Time: 236 ms, Memory: 17992 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,25 @@
+//scanner import
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        //scanner 선언
+        Scanner scanner = new Scanner(System.in);
+        
+        //T 입력 받기
+        int T = scanner.nextInt();
+        
+        //T만큼 숫자 입력 받기
+        for (int i = 0; i < T; i++){
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+            
+            //두 수 더한 값 출력
+            System.out.println(A + B);
+        }
+        
+        //scanner 닫기
+        scanner.close();
+
+    }
+}
