f8680ae6d5dbfa525488dc7d8b216dda17d14759
AYOUNG YANG <dkdudab@hanmail.net>
2022-03-28 20:47:21 +0900

    [Bronze II] Title: OX퀴즈, Time: 228 ms, Memory: 17808 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,41 @@
+//import
+import java.util.Scanner;
+ 
+public class Main {
+	public static void main(String[] args) {
+		//scanner 선언
+	    Scanner scanner = new Scanner(System.in);
+ 
+        //점수 결과 배열 선언
+		String array[] = new String[scanner.nextInt()];
+ 
+        //결과 입력 받아서 배열에 넣기
+		for (int i = 0; i < array.length; i++) {
+			array[i] = scanner.next();
+		}
+		
+        //각 결과 점수 계산하기
+		for (int i = 0; i < array.length; i++) {
+			
+			int count = 0;	// 연속 횟수
+			int sum = 0;	// 전체 점수 
+			
+			for (int j = 0; j < array[i].length(); j++) {
+				
+				if (array[i].charAt(j) == 'O')
+					count++;
+				else
+					count = 0;
+                
+                //전체 점수에 합하기
+				sum += count;
+			}
+			
+            //결과 출력
+			System.out.println(sum);
+		}
+        
+        //scanner 닫기
+	    scanner.close();
+	}
+}
