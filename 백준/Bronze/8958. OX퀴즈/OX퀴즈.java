2787a037fe3000794177e69e4a733ffdea6569c1
AYOUNG YANG <dkdudab@hanmail.net>
2022-04-02 19:23:02 +0900

    Update OX퀴즈.java

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -3,39 +3,39 @@ import java.util.Scanner;
  
 public class Main {
 	public static void main(String[] args) {
-		//scanner 선언
-	    Scanner scanner = new Scanner(System.in);
+	//scanner 선언
+	Scanner scanner = new Scanner(System.in);
  
         //점수 결과 배열 선언
-		String array[] = new String[scanner.nextInt()];
+	String array[] = new String[scanner.nextInt()];
  
         //결과 입력 받아서 배열에 넣기
-		for (int i = 0; i < array.length; i++) {
-			array[i] = scanner.next();
-		}
+	for (int i = 0; i < array.length; i++) {
+		array[i] = scanner.next();
+	}
 		
         //각 결과 점수 계산하기
-		for (int i = 0; i < array.length; i++) {
+	for (int i = 0; i < array.length; i++) {
 			
-			int count = 0;	// 연속 횟수
-			int sum = 0;	// 전체 점수 
+		int count = 0;	// 연속 횟수
+		int sum = 0;	// 전체 점수 
 			
-			for (int j = 0; j < array[i].length(); j++) {
+		for (int j = 0; j < array[i].length(); j++) {
 				
-				if (array[i].charAt(j) == 'O')
-					count++;
-				else
-					count = 0;
+			if (array[i].charAt(j) == 'O')
+				count++;
+			else
+				count = 0;
                 
                 //전체 점수에 합하기
-				sum += count;
-			}
-			
-            //결과 출력
-			System.out.println(sum);
+			sum += count;
 		}
+			
+            	//결과 출력
+		System.out.println(sum);
+	}
         
         //scanner 닫기
-	    scanner.close();
+	scanner.close();
 	}
 }
