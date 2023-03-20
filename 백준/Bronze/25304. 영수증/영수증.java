5867198af1199996f413ec5a303b656dd08f6843
AYOUNG YANG <dkdudab@hanmail.net>
2023-03-20 19:14:52 +0900

    [Bronze V] Title: 영수증, Time: 228 ms, Memory: 17800 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,36 @@
+//scanner import
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        //scanner 선언
+        Scanner scanner = new Scanner(System.in);
+        
+        //총 금액 입력 받기
+        int entire = scanner.nextInt();
+        
+        //갯수 입력 받기
+        int N = scanner.nextInt();
+        
+        //결과값 정의
+        int sum = 0;
+        String result = "No";
+       
+        for (int i = 0; i < N; i++){
+            int fee = scanner.nextInt();
+            int n = scanner.nextInt();
+            int s = fee * n;
+            sum += s;
+        }
+        
+        if (sum == entire)
+            result = "Yes";
+        
+        //결과 출력
+        System.out.println(result);
+        
+        //scanner 닫기
+        scanner.close();
+
+    }
+}
