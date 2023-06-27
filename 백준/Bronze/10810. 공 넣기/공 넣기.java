64e430b618c59b646f6b5ce8444dd3449eba4c84
AYOUNG YANG <dkdudab@hanmail.net>
2023-06-27 15:47:03 +0900

    [Bronze III] Title: 공 넣기, Time: 152 ms, Memory: 15936 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.java
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -1,25 +1,35 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException{
         
-        int N = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
         int[] arr = new int[N];
-        int M = sc.nextInt();
+        int M = Integer.parseInt(st.nextToken());
         
         for(int a = 0; a < M; a++) {
-            int i = sc.nextInt();
-            int j = sc.nextInt();
-            int k = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
             
             for(int b = i - 1; b < j; b++) {
                 arr[b] = k;
             }
         }
         for(int c = 0; c < arr.length; c++) {
-            System.out.print(arr[c] + " ");
+            bw.write(arr[c] + " ");
         }
+        br.close();
+        bw.flush();
+        bw.close();
     }
 
 }
