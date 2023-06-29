e4e8fe6f00f7200b35ff2e799e11f966fd3d2e17
AYOUNG YANG <dkdudab@hanmail.net>
2023-06-29 14:26:55 +0900

    [Bronze V] Title: A+B, Time: 124 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,25 +1,22 @@
-//scanner import
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main{
-    public static void main(String[] args){
-        //scanner 선언
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException{
+    
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       
+        StringTokenizer st = new StringTokenizer(br.readLine());
         
-        //첫 번째 숫자 입력 받기
-        int num1 = scanner.nextInt();
+        int num1 = Integer.parseInt(st.nextToken());
         
-        //두 번째 숫자 입력 받기
-        int num2 = scanner.nextInt();
+        int num2 = Integer.parseInt(st.nextToken());
         
-        //계산하기
         int result = num1 + num2;
         
-        //결과 출력
         System.out.println(result);
         
-        //scanner 닫기
-        scanner.close();
+        br.close();
 
     }
 }
