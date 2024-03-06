0e1a15a65b3b648609ffbc377fb5a75d16593202
AYOUNG YANG <dkdudab@hanmail.net>
2024-03-06 20:59:36 +0900

    [Bronze III] Title: 곱셈, Time: 204 ms, Memory: 17600 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -1,31 +1,18 @@
-//scanner import
 import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
-        //scanner 선언
-        Scanner scanner = new Scanner(System.in);
         
-        //(1) 숫자 입력 받기
-        int num1 = scanner.nextInt();
+        Scanner sc = new Scanner(System.in);
         
-        //(2) 숫자 입력 받기
-        int num2 = scanner.nextInt();
+        int A = sc.nextInt();
+        int B = sc.nextInt();
         
-        //(3) 결과 출력
-        System.out.println(num1 * (num2 % 10));
+        System.out.println(A * ((B % 100) % 10));
+        System.out.println(A * ((B % 100) / 10));
+        System.out.println(A * (B / 100));
+        System.out.println(A * B);
         
-        //(4) 결과 출력
-		System.out.println(num1 * (num2 % 100 / 10));
-        
-        //(5) 결과 출력
-		System.out.println(num1 * (num2 / 100));
-        
-        //(6) 결과 출력
-        System.out.println(num1 * num2);
-        
-        //scanner 닫기
-        scanner.close();
-
+        sc.close();
     }
 }
