93dcdafa4f230bfb47a88909d9e8e88e4c062538
AYOUNG YANG <dkdudab@hanmail.net>
2024-03-06 20:48:00 +0900

    [Bronze V] Title: 나머지, Time: 204 ms, Memory: 17652 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -1,33 +1,17 @@
-//Scanner import
 import java.util.Scanner;
 
 public class Main{
-    public static void main(String[] args){
-        //scanner 선언
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
         
-        //첫 번째 숫자 입력 받기
-        int A = scanner.nextInt();
+        Scanner sc = new Scanner(System.in);
         
-        //두 번째 숫자 입력 받기
-        int B = scanner.nextInt();
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
         
-        //세 번째 숫자 입력 받기
-        int C = scanner.nextInt();
-        
-        //(A+B)%C 결과 출력하기
-        System.out.println((A + B) % C);
-        
-        //((A%C) + (B%C))%C 결과 출력하기
-        System.out.println(((A % C) + (B % C)) % C);
-        
-        //(A×B)%C 결과 출력하기
-        System.out.println((A * B) % C);
-        
-        //((A%C) × (B%C))%C 결과 출력하기
-        System.out.println(((A % C) * (B % C)) % C);
-        
-        //scanner 닫기
-        scanner.close();
+        System.out.println((A+B)%C);
+        System.out.println(((A%C) + (B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C) * (B%C))%C);
     }
 }
