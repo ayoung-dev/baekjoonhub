5f7c65b71d6f6da29137b49e4c53d6c6d5b3a8f7
AYOUNG YANG <dkdudab@hanmail.net>
2024-03-07 16:42:32 +0900

    [Bronze V] Title: 시험 성적, Time: 128 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,24 +1,15 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int score = sc.nextInt();
+        int score = Integer.parseInt(br.readLine());
         
-        if (score >= 90 )
-            System.out.println("A");
-        else if (score >= 80)
-            System.out.println("B");
-        else if (score >= 70)
-            System.out.println("C");
-        else if (score >= 60)
-            System.out.println("D");
-        else
-            System.out.println("F");
+        System.out.println((score < 60) ? "F" : (score < 70) ? "D" : (score < 80) ? "C" : (score < 90) ? "B" : "A");
         
-        sc.close();
+        br.close();
             
     }
 }
