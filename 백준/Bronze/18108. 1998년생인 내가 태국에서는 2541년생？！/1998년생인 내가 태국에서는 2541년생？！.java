c5d6e6970a1c9915ec4dfa9dd760b54a4c7b17ff
AYOUNG YANG <dkdudab@hanmail.net>
2024-03-07 15:24:54 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -4,11 +4,13 @@ public class Main{
     public static void main(String[] args) throws IOException {
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
         int y = Integer.parseInt(br.readLine());
         
-        System.out.println(y - 543);
+        bw.write(String.valueOf(y - 543));
         
+        bw.flush();
         br.close();
     }
 }
