ddec375d6958176712440fbb2ca1866d4db4e67a
AYOUNG YANG <dkdudab@hanmail.net>
2024-03-08 21:53:48 +0900

    [Bronze III] Title: 알람 시계, Time: 240 ms, Memory: 18536 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -1,34 +1,27 @@
-//scanner import
 import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
-        //scanner 선언
-        Scanner scanner = new Scanner(System.in);
         
-        //H 입력 받기
-        int H = scanner.nextInt();
+        Scanner sc = new Scanner(System.in);
         
-        //M 입력 받기
-        int M = scanner.nextInt();
+        int H = sc.nextInt();
+        int M = sc.nextInt();
         
-        //알람 시간 계산
-        //만약 M이 45보다 작다면
         if (M < 45){
             H--;
             M = 60 - (45 - M);
+            
             //만약 H가 0보다 작다면
             if (H < 0)
                  H = 23; 
-            //알람 시간 출력
-            System.out.println(H+ " " +M);
-        } 
-        //둘 다 아니라면
-        else
-            System.out.println(H+ " " + (M - 45));
+
+            System.out.println(H + " " + M);
+            
+        } else
+            System.out.println(H + " " + (M - 45));
         
-        //scanner 닫기
-        scanner.close();
+        sc.close();
 
     }
 }
