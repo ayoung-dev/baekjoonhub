9377468de40a5fb8785b46645a6125e8503c8b4e
AYOUNG YANG <dkdudab@hanmail.net>
2024-03-08 21:13:01 +0900

    [Bronze V] Title: 윤년, Time: 124 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,17 +1,17 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int y = sc.nextInt();
+        int y = Integer.parseInt(br.readLine());
         
         if (y % 4 == 0 && y % 100 != 0 || y % 400 == 0)
             System.out.println(1);
         else
             System.out.println(0);
         
-        sc.close();
+        br.close();
     }
 }
