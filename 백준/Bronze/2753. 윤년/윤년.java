2c29c347f340a38ff6bbb826e4228b99e0a365bc
AYOUNG YANG <dkdudab@hanmail.net>
2024-03-08 21:09:30 +0900

    [Bronze V] Title: 윤년, Time: 208 ms, Memory: 17728 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,24 +1,17 @@
-//scanner import
 import java.util.Scanner;
 
-public class Main{
-    public static void main(String[] args){
-        //scanner 선언
-        Scanner scanner = new Scanner(System.in);
+public class Main {
+    public static void main(String[] args) {
         
-        //년도 입력 받기
-        int year = scanner.nextInt();
+        Scanner sc = new Scanner(System.in);
         
-        //4의 배수이면서 100의 배수가 아닐 때 & 400의 배수일 때
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
-            //맞으면 1 출력
+        int y = sc.nextInt();
+        
+        if (y % 4 == 0 && y % 100 != 0 || y % 400 == 0)
             System.out.println(1);
         else
-            //아니면 0 출력
             System.out.println(0);
         
-        //scanner 닫기
-        scanner.close();
-
+        sc.close();
     }
 }
