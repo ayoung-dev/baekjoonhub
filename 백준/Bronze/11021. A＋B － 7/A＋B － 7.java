b27b984f877ee73e26bfabfc440316ad588102f0
AYOUNG YANG <dkdudab@hanmail.net>
2024-03-11 16:31:37 +0900

    [Bronze V] Title: A+B - 7, Time: 164 ms, Memory: 16008 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -1,19 +1,22 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int T = sc.nextInt();
+        int T = Integer.parseInt(br.readLine());
         
         for (int i = 1; i < T + 1; i++) {
-            int A = sc.nextInt();
-            A += sc.nextInt();
+            StringTokenizer st = new StringTokenizer(br.readLine());
             
-            System.out.println("Case #" + i + ": " + A);
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            
+            System.out.println("Case #" + i + ": " + (A + B));
         }
         
-        sc.close();
+        br.close();
     }
 }
