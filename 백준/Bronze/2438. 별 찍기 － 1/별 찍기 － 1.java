efd9a25501635877062d298b3c78d5d720a6d92a
AYOUNG YANG <dkdudab@hanmail.net>
2024-03-11 17:13:47 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 120 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -4,19 +4,22 @@ public class Main {
     public static void main(String[] args) throws IOException {
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
         int N = Integer.parseInt(br.readLine());
         
         StringBuilder sb = new StringBuilder();
         
         for (int i = 1; i < N + 1; i++) {
-            for (int j = 1; j < i + 1; j++)
+            for (int j = 0; j < i; j++)
                 sb.append("*");
             sb.append("\n");
         }
         
-        System.out.print(sb);
+        bw.write(sb.toString());
         
         br.close();
+        bw.flush();
+        bw.close();
     }
 }
