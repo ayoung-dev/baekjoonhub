9066ba5388699288c797f307256f5837ce64d035
AYOUNG YANG <dkdudab@hanmail.net>
2024-03-14 14:46:06 +0900

    [Bronze II] Title: 나머지, Time: 128 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,44 +1,30 @@
-//import
-	import java.util.Scanner;
+import java.io.*;
 
-public class Main{
-	    public static void main(String[] args){
-	        //scanner 선언
-	        Scanner scanner = new Scanner(System.in);
-	        
-	        int count = 0;
-	        
-	        //배열 선언
-	        int[] array = new int[10];
-	        
-	        for (int i = 0; i < 10; i++){
-	            //숫자 입력 받기
-	            int in = (scanner.nextInt()) % 42;
-	        
-	            //배열에 넣기
-	            array[i] = in; 
-	        }
-	       
-	        
-	        for(int j = 0; j < array.length; j++){
-	            //break 체크용
-	            int check = 0;
-	            
-	            for (int k = j; k < array.length; k++){
-	                if (array[j] == array[k])
-	                    check++;
-	                
-	            }
-	            if(check < 2) 
-	            	count++;
-	                
-	        }
-	        
-	        //출력
-	        System.out.println(count);
-	        
-	        //scanner 닫기
-	        scanner.close();
-
-	  }
+public class Main {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int[] arr = new int[10];
+        int count = 0;
+        
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (Integer.parseInt(br.readLine()) % 42);
+        }
+        
+        for (int i = 0; i < arr.length; i++) {
+            boolean b = false;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    b = true;
+                    break;
+                }
+            }
+            if (b == false) {
+                count++;
+            }
+        }
+        System.out.println(count);
+        br.close();
+    }
 }
