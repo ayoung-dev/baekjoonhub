3e1e5303d9834baabccd0e93fca25539b4ccd676
AYOUNG YANG <dkdudab@hanmail.net>
2024-03-14 18:25:26 +0900

    [Bronze I] Title: 평균, Time: 260 ms, Memory: 19300 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -3,39 +3,39 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Main{
-	public static void main(String[] args){
-		//scanner 선언
-	      	Scanner scanner = new Scanner(System.in);
+	    public static void main(String[] args){
+	      //scanner 선언
+	      Scanner scanner = new Scanner(System.in);
 	        
-          	//과목 수 입력 받기
-	     	int sub = scanner.nextInt();
+          //과목 수 입력 받기
+	      int sub = scanner.nextInt();
 	        
-	      	//배열 선언
-	      	int[] array = new int[sub];
+	      //배열 선언
+	      int[] array = new int[sub];
           
-          	//성적 입력 받아서 배열에 넣기
+          //성적 입력 받아서 배열에 넣기
 	        for (int i = 0; i < sub; i++){
 	            //성적 입력 받기
 	            int in = scanner.nextInt();
 	        
 	            //배열에 넣기
 	            array[i] = in; 
-		}
+	        }
 	       
-          	//Max값 구하기
-         	Arrays.sort(array);
-          	double max = array[array.length - 1];
+          //Max값 구하기
+          Arrays.sort(array);
+          double max = array[array.length - 1];
 	        
-          	double sum = 0.0;
+          double sum = 0.0;
           
-	      	//배열 내 성적들 바꿔서 더하기
-		for(int j = 0; j < array.length; j++){
-              		double num = (array[j] / max) * 100;
-              		sum += num;
-          	}
+	      //배열 내 성적들 바꿔서 더하기
+          for(int j = 0; j < array.length; j++){
+              double num = (array[j] / max) * 100;
+              sum += num;
+          }
 	        
-          	//평균
-          	double avr = sum / array.length;
+          //평균
+          double avr = sum / array.length;
           
 	        //출력
 	        System.out.println(avr);
