1cf74d2484d8f99baa6034992bbc0bd11e336a16
AYOUNG YANG <dkdudab@hanmail.net>
2024-03-14 18:29:41 +0900

    [Bronze I] Title: 평균, Time: 132 ms, Memory: 14928 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,47 +1,33 @@
-//import
-import java.util.Scanner;
-import java.util.Arrays;
+import java.io.*;
+import java.util.StringTokenizer;
 
-public class Main{
-	    public static void main(String[] args){
-	      //scanner 선언
-	      Scanner scanner = new Scanner(System.in);
-	        
-          //과목 수 입력 받기
-	      int sub = scanner.nextInt();
-	        
-	      //배열 선언
-	      int[] array = new int[sub];
-          
-          //성적 입력 받아서 배열에 넣기
-	        for (int i = 0; i < sub; i++){
-	            //성적 입력 받기
-	            int in = scanner.nextInt();
-	        
-	            //배열에 넣기
-	            array[i] = in; 
-	        }
-	       
-          //Max값 구하기
-          Arrays.sort(array);
-          double max = array[array.length - 1];
-	        
-          double sum = 0.0;
-          
-	      //배열 내 성적들 바꿔서 더하기
-          for(int j = 0; j < array.length; j++){
-              double num = (array[j] / max) * 100;
-              sum += num;
-          }
-	        
-          //평균
-          double avr = sum / array.length;
-          
-	        //출력
-	        System.out.println(avr);
-	        
-	        //scanner 닫기
-	        scanner.close();
-
-	  }
+public class Main {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        double[] arr = new double[N];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        double max = Double.parseDouble(st.nextToken());
+        arr[0] = max;
+        double sum = 0;
+        
+        
+        
+        for (int i = 1; i < N; i++) { 
+            arr[i] = Double.parseDouble(st.nextToken());
+            max = Math.max(max, arr[i]);
+        }
+        
+        for (int i = 0; i < N; i++) {
+            arr[i] = arr[i]/max*100;
+            sum += arr[i];
+        }
+        
+        System.out.println(sum / N);
+        
+        br.close();
+    }
 }
