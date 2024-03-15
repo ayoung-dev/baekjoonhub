6c077852d588184b096a5185a1dab83967ef9c12
AYOUNG YANG <dkdudab@hanmail.net>
2024-03-15 20:20:59 +0900

    [Bronze V] Title: 문자열, Time: 124 ms, Memory: 13992 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.java
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -1,20 +1,20 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int T = sc.nextInt();
+        int T = Integer.parseInt(br.readLine());
         
         for (int i = 0; i < T; i++) {
-            String S = sc.next();
+            String S = br.readLine();
             
             //char + 연산자 사용하면 아스키코드값이 더해짐!
             System.out.print(S.charAt(0));
             System.out.println(S.charAt(S.length() - 1));
         }
         
-        sc.close();
+        br.close();
     }
 }
