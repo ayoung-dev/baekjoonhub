1e6862d397f906cd0ea0c38b0dd21db3e583ee4c
AYOUNG YANG <dkdudab@hanmail.net>
2024-03-15 20:01:17 +0900

    [Bronze V] Title: 문자와 문자열, Time: 120 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -1,14 +1,15 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String st = br.readLine();
-		int n = Integer.parseInt(br.readLine());
-		
-		System.out.println(st.charAt(n-1));
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String S = br.readLine();
+        int i = Integer.parseInt(br.readLine());
+        
+        System.out.println(S.charAt(i - 1));
         
         br.close();
-	}
+    }
 }
