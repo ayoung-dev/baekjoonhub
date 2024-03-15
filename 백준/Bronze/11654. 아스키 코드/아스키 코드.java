81b176a98f1541d8beb8ca74d12747c74d92a055
AYOUNG YANG <dkdudab@hanmail.net>
2024-03-15 20:33:00 +0900

    [Bronze V] Title: 아스키 코드, Time: 196 ms, Memory: 17624 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -1,16 +1,12 @@
-//import
 import java.util.Scanner;
- 
+
 public class Main {
-	public static void main(String[] args) {
- 
-        //scanner 선언
-		Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
         
-        //입력받은 문자 아스키코드(int)로 바꿔서 입력 받기
-		int asc = scanner.next().charAt(0);
+        Scanner sc = new Scanner(System.in);
         
-        //아스키코드 값 출력
-		System.out.print(asc);
-	}
+        int out = sc.next().charAt(0);
+        
+        System.out.println(out);
+    }
 }
