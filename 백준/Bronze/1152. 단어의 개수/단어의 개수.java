1022a196a8b6437c2e42495ad839b4855942f62e
AYOUNG YANG <dkdudab@hanmail.net>
2024-03-17 21:51:45 +0900

    [Bronze II] Title: 단어의 개수, Time: 248 ms, Memory: 20004 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,27 +1,15 @@
-//import
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-
-      //scanner 선언
-	  Scanner scanner = new Scanner(System.in);
-      
-      //문장 입력 받기 
-      //.trim()을 이용하여 앞 뒤 공백 제거
-      String Str = scanner.nextLine().trim();
+    public static void main(String[] args) throws IOException {
         
-      //scanner 닫기
-      scanner.close();
-
-      //공백으로 단어 구분해서 문자열에 넣기 
-      String[] result = Str.split(" ");
-
-      //결과 출력
-      if (Str.isEmpty())
-          System.out.println(0);
-      else
-          System.out.println(result.length);
-  
-	}
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        System.out.println(st.countTokens());
+        
+        br.close();
+    }
 }
