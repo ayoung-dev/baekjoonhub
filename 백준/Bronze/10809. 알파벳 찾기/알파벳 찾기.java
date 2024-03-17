8a30795ecdf9ec28793cf5a1172ed62d4da95ff3
AYOUNG YANG <dkdudab@hanmail.net>
2024-03-17 20:49:37 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 224 ms, Memory: 18260 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,38 +1,15 @@
-//import
 import java.util.Scanner;
-import java.util.Arrays;
- 
+
 public class Main {
-	public static void main(String[] args) {
- 
-        //scanner 선언
-		Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
         
-        //S 입력받기 
-        String s = scanner.next();
+        Scanner sc = new Scanner(System.in);
         
-        //위치 넣어둘 배열 생성
-        int[] array = new int[26]; 
+        String S = sc.next();
         
-        //array 모든 element -1로 초기화
-        Arrays.fill(array, -1);
-        
-        for(int i = 0; i < s.length(); i++){
-            //S 하나씩 잘라서
-            char ch = s.charAt(i);
-            
-            //만약 값이 -1 이라면! (첫 번째 나타난 위치를 받기 위해)
-            if(array[ch - 'a'] == -1)
-                //index값 해당 배열의 값으로 넣어주기
-		        array[ch - 'a'] = i;
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            System.out.print(S.indexOf(ch) + " ");
         }
-            
-        //결과 출력
-        for(int i = 0; i < array.length; i++) {	
-			System.out.print(array[i] + " ");
-		}
-        
-        //scanner 닫기
-        scanner.close();
-	}
+        sc.close();       
+    }
 }
