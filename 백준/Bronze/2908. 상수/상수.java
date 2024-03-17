ec3cf56e9decf034c7d49bcbb1414af62dc4ebaf
AYOUNG YANG <dkdudab@hanmail.net>
2024-03-17 22:05:04 +0900

    [Bronze II] Title: 상수, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -1,26 +1,20 @@
-//import
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-
-    //scanner 선언
-	Scanner scanner = new Scanner(System.in);
-    
-	//숫자 2개 입력 받기 
-	int A = scanner.nextInt();
-	int B = scanner.nextInt();
-    
-	//scanner 닫기 
-    scanner.close();
-    
-    //int 값 --> StringBuilder를 통해 reverse 함수를 사용하여 뒤집기
-    //뒤집은 값 --> String --> int
-	A = Integer.parseInt(new StringBuilder().append(A).reverse().toString());
-	B = Integer.parseInt(new StringBuilder().append(B).reverse().toString());
-	
-	//둘 중 큰 값 출력  
-	System.out.print(A > B ? A : B);
-	
-	}
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        
+        A = Integer.parseInt(new StringBuilder().append(A).reverse().toString());
+        B = Integer.parseInt(new StringBuilder().append(B).reverse().toString());
+        
+        System.out.println(A > B ? A : B);
+        
+        br.close();
+    }
 }
