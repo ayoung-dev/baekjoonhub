4c4ebe743257fde2923b3ab6cadc7533f1c06654
AYOUNG YANG <dkdudab@hanmail.net>
2024-03-17 20:52:54 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 140 ms, Memory: 15936 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -4,12 +4,16 @@ public class Main {
     public static void main(String[] args) throws IOException {
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         
         String S = br.readLine();
         
         for (char ch = 'a'; ch <= 'z'; ch++) {
-            System.out.print(S.indexOf(ch) + " ");
+            sb.append(S.indexOf(ch) + " ");
         }
+        
+        System.out.println(sb);
+        
         br.close();       
     }
 }
