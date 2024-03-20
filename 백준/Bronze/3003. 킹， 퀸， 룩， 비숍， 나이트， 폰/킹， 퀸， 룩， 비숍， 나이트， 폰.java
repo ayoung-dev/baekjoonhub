166f2d9852fed025107fe5dfb5b897204df0163d
AYOUNG YANG <dkdudab@hanmail.net>
2024-03-20 20:48:47 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 140 ms, Memory: 15844 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -2,21 +2,17 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		
-		int[] chess = {1, 1, 2, 2, 2, 8};
-		
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
-		
-		int[] out = new int[6];
-		
-		for (int i = 0; i < chess.length; i++) {
-			
-			out[i] = chess[i] - Integer.parseInt(st.nextToken());
-			
-			System.out.print(out[i] + " ");
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int[] chess = {1, 1, 2, 2, 2, 8};
+        
+        for (int i = 0; i < 6; i++) {
+           System.out.print((chess[i] - Integer.parseInt(st.nextToken())) + " ");
+        }
+        
+        br.close();
+    }
 }
