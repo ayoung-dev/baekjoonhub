b15e18790d8a5fb09231fe5c73f50cffd5e72c22
AYOUNG YANG <dkdudab@hanmail.net>
2024-03-22 21:10:46 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 200 ms, Memory: 17584 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -1,20 +1,19 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		String str = sc.next();
-		sc.close();
-		int len = str.length();
-		int ans = 1;
-		
-		for(int i=0;i<len/2;i++) {
-			if(str.charAt(i)!=str.charAt(len-1-i)) {
-				ans = 0;
-			}
-		}
-		System.out.println(ans);
-		
-	}
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append(sc.next());
+        
+        if (sb.toString().equals(sb.reverse().toString())) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+
+        sc.close();
+    }
 }
