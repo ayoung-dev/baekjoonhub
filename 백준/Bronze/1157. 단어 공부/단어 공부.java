97d752061920738127f4682b7cff280c00d84968
AYOUNG YANG <dkdudab@hanmail.net>
2024-03-24 19:28:21 +0900

    [Bronze I] Title: 단어 공부, Time: 280 ms, Memory: 21064 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,39 +1,29 @@
-import java.util.Scanner;
- 
+import java.io.*;
+
 public class Main {
- 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
- 
-		int[] arr = new int[26]; // 영문자의 개수는 26개임
-		String s = in.next();
- 
-		for (int i = 0; i < s.length(); i++){
- 
-			if ('A' <= s.charAt(i) && s.charAt(i) <= 'Z') { // 대문자 범위
-				arr[s.charAt(i) - 'A']++;	// 해당 인덱스의 값 1 증가
-			}
-    
-			else {	// 소문자 범위
-				arr[s.charAt(i) - 'a']++;
-			}
-		}
- 
- 
-		int max = -1;
-		char ch = '?';
- 
-		for (int i = 0; i < 26; i++) {
-			if (arr[i] > max) {
-				max = arr[i];
-				ch = (char) (i + 65); // 대문자로 출력해야하므로 65를 더해준다.
-			}
-			else if (arr[i] == max) {
-				ch = '?';
-			}
-		}
- 
-		System.out.print(ch);
-	}
- 
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int[] arr = new int[26];
+        String S = br.readLine().toUpperCase();
+        
+        for (int i = 0; i < S.length(); i++) {
+            arr[S.charAt(i) - 'A']++;
+        }
+        
+        int max = -1;
+        char ch = '?';
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                ch = (char) (i + 'A');
+            } else if (arr[i] == max) {
+                ch = '?';
+            }
+        }
+        System.out.println(ch);
+        br.close();
+    }
 }
