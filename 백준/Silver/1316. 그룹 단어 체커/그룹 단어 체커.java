3acaf5c8d29aee1bf59f9f6d8766a7a8bf2f4b0e
AYOUNG YANG <dkdudab@hanmail.net>
2024-03-25 20:46:10 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 128 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,53 +1,41 @@
-import java.util.Scanner;
- 
+import java.io.*;
+
 public class Main {
- 
-	static Scanner in = new Scanner(System.in);
- 
-	public static void main(String[] args) {
- 
-		int count = 0;
-		int N = in.nextInt();
- 
-		for (int i = 0; i < N; i++) {
-			if (check() == true) {
-				count++;
-			}
-		}
-		System.out.println(count);
-	}
- 
-	public static boolean check() {
-		boolean[] check = new boolean[26];
-		int prev = 0;
-		String str = in.next();
-		
-		for(int i = 0; i < str.length(); i++) {
-			int now = str.charAt(i);	// i 번째 문자 저장 (현재 문자)
-			
-			
-			// 앞선 문자와 i 번째 문자가 같지 않다면?
-			if (prev != now) {		
-				
-				// 해당 문자가 처음 나오는 경우 (false 인 경우)
-				if ( check[now - 'a'] == false ) {
-					check[now - 'a'] = true;		// true 로 바꿔준다
-					prev = now;					// 다음 턴을 위해 prev 도 바꿔준다 
-				}
-	 
-				// 해당 문자가 이미 나온 적이 있는 경우 (그룹단어가 아니게 됨) 
-				else {
-					return false;	//함수 종료
-				}
-			}
-	        
-	        
-			// 앞선 문자와 i 번째 문자가 같다면? (연속된 문자)
-			// else 문은 없어도 됨
-			else {
-				continue;
-			}
-		}    
-		return true;
-	}
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+        
+        for (int i = 0; i < N; i++) {
+            if (check(br.readLine())) {
+            	count++;
+            }
+        }
+        System.out.println(count);
+        
+        br.close();
+    }
+    
+    static boolean check(String str) {
+        boolean[] arr = new boolean[26];
+        int prev = -1;
+    
+        for (int i = 0; i < str.length(); i++) {
+            int now = str.charAt(i);
+        
+            if (prev != now) {
+                if(arr[now - 'a'] == false) {
+                    arr[now - 'a'] = true;
+                    prev = now;
+                } else {
+                    return false;
+                }
+            } else {
+                continue;
+            }
+        }
+        return true;
+    }
 }
