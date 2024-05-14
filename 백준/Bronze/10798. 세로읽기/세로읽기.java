028897e5a4567542bb780b1e5354512ed0e1702d
AYOUNG YANG <dkdudab@hanmail.net>
2024-05-14 20:40:49 +0900

    [Bronze I] Title: 세로읽기, Time: 128 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
         char[][] arr = new char[5][15];
         
@@ -19,8 +20,12 @@ public class Main {
             for (int j = 0; j < 5; j++) {
                 if (arr[j][i] == '\0')
                     continue;
-                System.out.print(arr[j][i]);
+                bw.write(arr[j][i]);
             }
         }
+        
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
