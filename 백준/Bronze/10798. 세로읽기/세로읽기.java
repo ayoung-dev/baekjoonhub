66712da68db8c16890197f0d6ce7dec602008311
AYOUNG YANG <dkdudab@hanmail.net>
2024-05-14 20:39:28 +0900

    [Bronze I] Title: 세로읽기, Time: 132 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -1,29 +1,26 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		char[][] str = new char[5][15];
-
-		String input = "";
-		for (int i = 0; i < str.length; i++) {
-			input = sc.next();
-
-			for (int j = 0; j < input.length(); j++) {
-				str[i][j] = input.charAt(j);
-			}
-		}
-
-		for (int i = 0; i < 15; i++) { 
-			for (int j = 0; j < 5; j++) { 
-				if (str[j][i] == '\0') 
-					continue;
-				System.out.print(str[j][i]);
-
-			}
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        char[][] arr = new char[5][15];
+        
+        for (int i = 0; i < arr.length; i++) {       
+            String str = br.readLine();
+            
+            for (int j = 0; j < str.length(); j++) {
+                arr[i][j] = str.charAt(j);
+            } 
+        }
+        
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (arr[j][i] == '\0')
+                    continue;
+                System.out.print(arr[j][i]);
+            }
+        }
+    }
 }
