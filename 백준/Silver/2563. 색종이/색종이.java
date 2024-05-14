08ecf2e652e1ca59cd70e14697c46f8c7086ca1e
AYOUNG YANG <dkdudab@hanmail.net>
2024-05-14 20:53:04 +0900

    [Silver V] Title: 색종이, Time: 220 ms, Memory: 17716 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,25 +1,28 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		boolean[][] board = new boolean[100][100];
-		while(N-- > 0) {
-			int w = sc.nextInt();
-			int h = sc.nextInt();
-			for(int i = w; i - w < 10; i++) {
-				for(int j = h; j - h < 10; j++)
-					board[i][j] = true;
-			}
-		}
-		
-		int sum = 0;
-		for(int i = 0; i < board.length; i++) {
-			for(int j = 0; j < board.length; j++) {
-				if(board[i][j])
-					sum++;
-			}
-		}
-		System.out.println(sum);
-	}
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int width = 0;
+        
+        boolean[][] arr = new boolean[100][100];
+        
+        for (int i = 0; i < N; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            
+            for (int j = x; j < x + 10; j ++) {
+                for (int k = y; k < y + 10; k++) {
+                    if (!arr[j][k]) {
+                        arr[j][k] = true;
+                        width++;
+                    }
+                }
+            }
+        }
+        System.out.println(width);
+    }
 }
