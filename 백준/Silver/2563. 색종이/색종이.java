41b58137e07564776a78c6add08a26ffc2ea2930
AYOUNG YANG <dkdudab@hanmail.net>
2024-05-15 16:11:33 +0900

    [Silver V] Title: 색종이, Time: 124 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,18 +1,22 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int N = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
         int width = 0;
         
         boolean[][] arr = new boolean[100][100];
         
         for (int i = 0; i < N; i++) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
+            
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
             
             for (int j = x; j < x + 10; j ++) {
                 for (int k = y; k < y + 10; k++) {
@@ -24,5 +28,7 @@ public class Main {
             }
         }
         System.out.println(width);
+        
+        br.close();
     }
 }
