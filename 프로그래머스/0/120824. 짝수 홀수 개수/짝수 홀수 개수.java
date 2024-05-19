df1c20e6f92c8fe5e0133dbebeecab52ff436816
AYOUNG YANG <dkdudab@hanmail.net>
2024-05-20 01:25:20 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -1,20 +1,16 @@
 class Solution {
     public int[] solution(int[] num_list) {
         
-        int add = 0, even = 0;
         int[] answer = new int[2];
         
         for (int i: num_list) {
             if (i % 2 == 0) {
-                even++;
+                answer[0]++;
             } else {
-                add++;
+                answer[1]++;
             }
         }
-        
-        answer[0] = even;
-        answer[1] = add;
-        
+
         return(answer);
     }
 }
