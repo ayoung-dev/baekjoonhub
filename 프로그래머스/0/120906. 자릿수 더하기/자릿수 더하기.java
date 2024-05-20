674609ee235affd6aac563125b3f50b84e7501ba
AYOUNG YANG <dkdudab@hanmail.net>
2024-05-20 18:05:55 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -1,14 +1,12 @@
 class Solution {
     public int solution(int n) {
-        
-        String s = Integer.toString(n);
-        
         int answer = 0;
-        
-        for (char c: s.toCharArray()) {
-            answer += (int) c - '0';
+
+        while(n > 0){
+            answer += n % 10;
+            n /= 10;
         }
-        
+
         return answer;
     }
 }
