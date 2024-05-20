fc2d51160115260c87d670c1a56264ee46ce6a47
AYOUNG YANG <dkdudab@hanmail.net>
2024-05-20 13:43:39 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        
+        int answer = 1;
+        
+        while (n > 7) {
+            answer++;
+            n -= 7;
+        }
+        
+        return answer;
+    }
+}
