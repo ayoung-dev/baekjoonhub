7ea43e0638505df4a4b4acc544a2c7e652f4153e
AYOUNG YANG <dkdudab@hanmail.net>
2024-05-20 20:54:55 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.03 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -1,11 +1,10 @@
 class Solution {
     public int[] solution(int n, int k) {
         int[] answer = new int [n / k];
-        int count = 0;
+        int idx = 0;
         
         for (int i = k; i <= n; i += k) {
-            answer[count] = i;
-            count++;
+            answer[idx++] = i;
         }
         return answer;
     }
