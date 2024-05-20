fed7ed71f365965c2e0a4e65330ff30f28210211
AYOUNG YANG <dkdudab@hanmail.net>
2024-05-20 15:04:38 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.02 ms, Memory: 70.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String my_string) {
+        
+        int answer = 0;
+        
+        for (int i = 0; i < my_string.length(); i++){
+            int num = (int) my_string.charAt(i) - '0';
+            if (num < 10) {
+                answer += num;
+            }
+        }
+        
+        return answer;
+    }
+}
