828a543552a751474f98f96b9cb5ddc8fb7fce74
AYOUNG YANG <dkdudab@hanmail.net>
2024-05-20 14:53:32 +0900

    [level 0] Title: 모음 제거, Time: 13.06 ms, Memory: 63.8 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -2,7 +2,19 @@ import java.util.*;
 
 class Solution {
     public String solution(String my_string) {
+        //return my_string.replaceAll("[aeiou]", "");
         
-        return my_string.replaceAll("[aeiou]", "");
+        String answer = "";
+        
+        ArrayList<Character> aeiou = new ArrayList<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+        
+        for (int i = 0; i < my_string.length(); i++) {
+            char c = my_string.charAt(i);
+            
+            if (!aeiou.contains(c))
+                answer += c;
+        }
+        
+        return answer;
     }
 }
