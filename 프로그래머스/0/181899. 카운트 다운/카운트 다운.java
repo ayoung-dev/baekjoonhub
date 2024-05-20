a042dc28cc23e6dae1acd7f1cad88ea333e4fbc9
AYOUNG YANG <dkdudab@hanmail.net>
2024-05-20 20:52:10 +0900

    [level 0] Title: 카운트 다운, Time: 0.02 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -1,11 +1,9 @@
 class Solution {
     public int[] solution(int start, int end_num) {
         int[] answer = new int[start - end_num + 1];
-        int num = start;
         
         for (int i = 0; i <= start - end_num; i++) {
-            answer[i] = num;
-            num--;
+            answer[i] = start - i;
         }
         return answer;
     }
