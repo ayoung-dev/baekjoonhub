ff462f2d4db4d6e8ff0742a9713a0de4591059fc
AYOUNG YANG <dkdudab@hanmail.net>
2024-05-21 10:23:14 +0900

    [level 0] Title: 더 크게 합치기, Time: 1.44 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(int a, int b) {
+        
+        return Math.max(Integer.parseInt(Integer.toString(a) + Integer.toString(b)),
+                       Integer.parseInt(Integer.toString(b) + Integer.toString(a)));
+    }
+}
