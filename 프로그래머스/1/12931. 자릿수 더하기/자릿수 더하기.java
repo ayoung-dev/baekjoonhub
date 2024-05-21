26ba66b4f8ab6709f183370ce9932dad5e62f0eb
AYOUNG YANG <dkdudab@hanmail.net>
2024-05-21 17:39:14 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int solution(int n) {
+        String str = Integer.toString(n);
+        int sum = 0;
+        
+        for (char ch: str.toCharArray()) {
+            sum += Character.getNumericValue(ch);
+        }
+        return sum;
+    }
+}
