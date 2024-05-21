bae911d3cf3d482f8680cbd0137a14741f7d02cb
AYOUNG YANG <dkdudab@hanmail.net>
2024-05-21 10:42:53 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.02 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -2,6 +2,6 @@ class Solution {
     public int solution(int a, int b) {
         if ((a + b) % 2 != 0) return 2*(a+b);
         else if (a % 2 != 0) return a*a + b*b;
-        else return (int) Math.sqrt(Math.pow(a-b,2));
+        else return (int) Math.abs(a-b);
     }
 }
