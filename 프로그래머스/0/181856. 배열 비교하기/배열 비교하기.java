25cb11bb2610307206aaab8c56bc7cb44174c103
AYOUNG YANG <dkdudab@hanmail.net>
2024-05-21 16:09:31 +0900

    [level 0] Title: 배열 비교하기, Time: 0.03 ms, Memory: 70.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] arr1, int[] arr2) {
+        if (arr1.length == arr2.length) {
+            if (Arrays.stream(arr1).sum() == Arrays.stream(arr2).sum()) return 0;
+                return Arrays.stream(arr1).sum() > Arrays.stream(arr2).sum() ? 1 : -1;
+        }
+        return arr1.length > arr2.length ? 1 : -1;
+    }
+}
