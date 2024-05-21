ae0de10854b9e5dda9e66cc102d2b7e608620e7d
AYOUNG YANG <dkdudab@hanmail.net>
2024-05-22 00:14:29 +0900

    [level 1] Title: 평균 구하기, Time: 2.38 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -2,6 +2,6 @@ import java.util.Arrays;
 
 class Solution {
     public double solution(int[] arr) {
-       return Arrays.stream(arr).sum() / (double) arr.length;
+       return Arrays.stream(arr).average().getAsDouble();
     }
 }
