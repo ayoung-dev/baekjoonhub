5848a8cbdca7aa75945af5d9c7f95d2c0a6afbbf
AYOUNG YANG <dkdudab@hanmail.net>
2024-05-21 13:46:52 +0900

    [level 0] Title: 0 떼기, Time: 0.03 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -1,5 +1,7 @@
 class Solution {
     public String solution(String n_str) {
         return Integer.toString(Integer.parseInt(n_str));
+        // return "" + Integer.parseInt(n_str);
+        // return String.valueOf(Integer.parseInt(n_str));
     }
 }
