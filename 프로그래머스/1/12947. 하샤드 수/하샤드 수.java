2e39e6e70f7e56606510b551b65e79ab957e4f38
AYOUNG YANG <dkdudab@hanmail.net>
2024-05-22 00:20:37 +0900

    [level 1] Title: 하샤드 수, Time: 0.17 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean solution(int x) {
+        String[] arr = String.valueOf(x).split("");
+        int sum = 0;
+        
+        for (String s: arr) {
+            sum += Integer.parseInt(s);
+        }
+        
+        return x % sum == 0;
+    }
+}
