7a8741f0f3b07f338bc8fe4a4b95e715b63f7218
AYOUNG YANG <dkdudab@hanmail.net>
2024-05-21 10:25:39 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.07 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String my_string, int k) {
+        StringBuilder answer = new StringBuilder();
+        
+        while (k > 0) {
+            answer.append(my_string);
+            k--;
+        }
+        
+        return answer.toString();
+    }
+}
