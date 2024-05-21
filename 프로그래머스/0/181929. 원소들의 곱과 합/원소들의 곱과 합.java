362ba0332fd8f3858d52dbf17d7ce968076ead86
AYOUNG YANG <dkdudab@hanmail.net>
2024-05-21 09:52:38 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.02 ms, Memory: 70.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] num_list) {
+        
+        int mul = 1, sum = 0;
+        
+        for (int i: num_list) {
+            mul *= i;
+            sum += i;
+        }
+        
+        return mul > sum * sum ? 0 : 1;
+    }
+}
