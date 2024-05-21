3dfcc1b75b766073841ff34d35968294b4b74f70
AYOUNG YANG <dkdudab@hanmail.net>
2024-05-21 16:00:36 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.05 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,8 +1,6 @@
 class Solution {
     public int solution(String myString, String pat) {
-        pat = pat.replace("A", "C");
-        pat = pat.replace("B", "A");
-        pat = pat.replace("C", "B");
+        pat = pat.replace("A", "C").replace("B", "A").replace("C", "B");
         
         return myString.contains(pat) ? 1 : 0;
         
