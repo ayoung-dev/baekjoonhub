44488e8e8c8ffd03753e25f1cc08a7cc11c30477
AYOUNG YANG <dkdudab@hanmail.net>
2024-05-23 20:27:10 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int num) {
+        long n = num;
+        
+        if (n == 1) return 0;
+        
+        for (int i = 0; i < 500; i++) 
+        {
+            if (n % 2 == 0) n /= 2;
+            else n = n * 3 + 1;
+
+            if (n == 1) return ++i;
+        }
+        
+        return -1;  
+
+    }
+}
