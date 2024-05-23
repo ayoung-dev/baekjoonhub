b1eae9c227e7408ec7de0c82c2b333fed6beeb29
AYOUNG YANG <dkdudab@hanmail.net>
2024-05-23 23:33:32 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.03 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int left, int right) {
+        int sum = 0, count = 0;
+        
+        for (int i = left; i <= right; i++) {
+            if (Math.sqrt(i) == (int) Math.sqrt(i)) sum -= i;
+            else sum += i;
+        }
+        
+        return sum;
+    }
+}
