29eac5502437b417555e881455e9c244e207ae4f
AYOUNG YANG <dkdudab@hanmail.net>
2024-05-28 16:41:14 +0900

    [level 0] Title: 할 일 목록, Time: 0.03 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+     List<String> list = new ArrayList<>();
+        
+        for (int i = 0; i < finished.length; i++) {
+            if (!finished[i])
+                list.add(todo_list[i]);
+        }
+        
+        String[] arr = list.toArray(new String[list.size()]);
+        
+        return arr;
+    }
+}
