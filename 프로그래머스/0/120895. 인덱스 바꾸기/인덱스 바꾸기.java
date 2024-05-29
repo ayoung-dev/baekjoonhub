66a3b790defcc8c99e14af0ab00dffaf0e135ffc
AYOUNG YANG <dkdudab@hanmail.net>
2024-05-30 02:07:16 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.19 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String[] arr = my_string.split("");
+        String tmp = arr[num1];
+        arr[num1] = arr[num2];
+        arr[num2] = tmp;
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (String s: arr) sb.append(s);
+        
+        return sb.toString();
+        }
+}
