3ed5ad67426e2a57851a7b74903a3e2f163cdd6d
AYOUNG YANG <dkdudab@hanmail.net>
2024-05-29 20:44:17 +0900

    [level 0] Title: 배열의 유사도, Time: 0.02 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        int count = 0;
+        
+        for (String s: s1) {
+            for (String st: s2) {
+                if (s.equals(st)) count++;
+            }
+        }
+        return count;
+    }
+}
