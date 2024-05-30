ef7bbc4566dde41ee181ad0874de3a2f653f5484
AYOUNG YANG <dkdudab@hanmail.net>
2024-05-30 16:43:36 +0900

    [level 0] Title: 약수 구하기, Time: 1.91 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -1,15 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int n) {
-        List<Integer> list = new ArrayList<>();
-        
-        for (int i = 1; i <= n; i++) {
-            if (n % i == 0) list.add(i);
-        }
-        
-        int[] arr = list.stream().mapToInt(x->x).toArray();
-        
-        return arr;
+        int[] answer = {};
+        return answer;
     }
 }
