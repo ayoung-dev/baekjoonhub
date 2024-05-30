11292a1922e184c724b63b3c70278c8cfc4e80b4
AYOUNG YANG <dkdudab@hanmail.net>
2024-05-30 16:49:04 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 1.87 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> list = new ArrayList<>();
+        
+        while (n != 1) {
+            list.add(n);
+            if (n % 2 == 0) n /= 2;
+            else n = 3 * n + 1;
+        }
+        
+        list.add(n);
+        
+        int[] arr = list.stream().mapToInt(x->x).toArray();
+        
+        return arr;
+
+    }
+}
