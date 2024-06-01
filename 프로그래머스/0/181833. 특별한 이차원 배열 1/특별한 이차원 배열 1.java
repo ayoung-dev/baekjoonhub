ea3867d1122b5898147bd9c820e14b7fa41543b7
AYOUNG YANG <dkdudab@hanmail.net>
2024-06-01 17:16:49 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.07 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.java
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.java
@@ -3,13 +3,7 @@ class Solution {
         int[][] answer = new int[n][n];
         
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == j){
-                    answer[i][j] = 1;
-                    break;
-                }
-                
-            }
+            answer[i][i] = 1;
         }
         
         return answer;
