e87bb42a9241b037661d48592f8c96d42eed2d7e
AYOUNG YANG <dkdudab@hanmail.net>
2024-06-03 19:19:15 +0900

    [level 0] Title: 문자열 돌리기, Time: 120.61 ms, Memory: 60 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String str = br.readLine();
+        br.close();
+        int idx = 0;
+        
+        while (idx < str.length()) {
+            System.out.println(str.charAt(idx++));
+        }
+
+    }
+}
