c9b7fd0a36e4a3e391bb1674badd9d5458a1a1d5
AYOUNG YANG <dkdudab@hanmail.net>
2024-06-13 16:51:48 +0900

    [level 0] Title: 모스부호 (1), Time: 0.18 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String letter) {
+        List<String> morse = new ArrayList<>(Arrays.asList(".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---",
+                         "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-",
+                         "..-", "...-", ".--", "-..-", "-.--", "--.."));
+        
+        String[] arr = letter.split(" ");
+        StringBuilder sb = new StringBuilder();
+        
+        for (String s: arr) {
+            sb.append((char) (morse.indexOf(s) + 'a'));
+        }
+        
+        return sb.toString();
+    }
+}
