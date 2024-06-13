961d3d8a365471da5afc074662033c6860e21766
AYOUNG YANG <dkdudab@hanmail.net>
2024-06-13 16:56:15 +0900

    [level 0] Title: 모스부호 (1), Time: 0.21 ms, Memory: 69.9 MB -BaekjoonHub

--- 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -2,9 +2,8 @@ import java.util.*;
 
 class Solution {
     public String solution(String letter) {
-        List<String> morse = new ArrayList<>(Arrays.asList(".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---",
-                         "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-",
-                         "..-", "...-", ".--", "-..-", "-.--", "--.."));
+        List<String> morse = new ArrayList<>(Arrays.asList(".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.",
+                                                           "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."));
         
         String[] arr = letter.split(" ");
         StringBuilder sb = new StringBuilder();
