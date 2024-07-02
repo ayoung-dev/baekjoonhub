d63ed79b3e140974f4014399f850d45a40ee871f
AYOUNG YANG <dkdudab@hanmail.net>
2024-07-03 01:41:30 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,5 +1,5 @@
 function solution(n) {
     var answer = [];
-    for (let i = 1; i <= n; i += 2) answer[Math.floor(i / 2)] = i;
+    for (let i = 1; i <= n; i += 2) answer.push(i);
     return answer;
 }
