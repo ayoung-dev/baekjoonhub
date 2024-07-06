092f3378dab8b0b8039bd56867c8d3259d407c5b
AYOUNG YANG <dkdudab@hanmail.net>
2024-07-06 10:40:57 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,9 +1,4 @@
 function solution(phone_number) {
-    var answer = '';
-    
-    for (let i = 0; i < phone_number.length - 4; i++) answer += '*';
-    
-    answer += phone_number.slice(-4);
-    
-    return answer;
+    var result = "*".repeat(phone_number.length - 4) + phone_number.slice(-4);
+    return result;
 }
