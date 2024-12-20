7206672c622db46110da59b6c7caf702f3101e6f
AYOUNG YANG <dkdudab@hanmail.net>
2024-12-20 17:05:33 +0900

    [level 0] Title: 커피 심부름, Time: 3.42 ms, Memory: 88.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String[] order) {
+        int answer = 0;
+        for (String menu: order) {
+            if (menu.contains("latte"))
+                answer += 5000;
+            else answer += 4500;
+        }
+        
+        return answer;
+    }
+}
