e014ea2cd8794838f8471151ba7daacca6ba0a0a
AYOUNG YANG <dkdudab@hanmail.net>
2025-03-07 11:03:41 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.16 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -1,57 +1,74 @@
 class Solution {
     public long solution(String numbers) {
-        int num = 0;
-        StringBuilder answer = new StringBuilder();
-        
-        while(num < numbers.length()) {
-            char ch = numbers.charAt(num);
-            
-            switch (ch) {
-                case 'z':
-                    answer.append("0");
-                    num += 4;
-                    break;
-                case 'o':
-                    answer.append("1");
-                    num += 3;
-                    break;
-                case 't':
-                    if (numbers.charAt(num + 1) == 'w') {
-                        answer.append("2");
-                        num += 3;
-                    } else {
-                        answer.append("3");
-                        num += 5;
-                    }
-                    break;
-                case 'f':
-                    if (numbers.charAt(num + 1) == 'o') {
-                        answer.append("4");
-                        num += 4;
-                    } else {
-                        answer.append("5");
-                        num += 4;
-                    }
-                    break;
-                case 's':
-                    if (numbers.charAt(num + 1) == 'i') {
-                        answer.append("6");
-                        num += 3;
-                    } else {
-                        answer.append("7");
-                        num += 5;
-                    }
-                    break;
-                case 'e':
-                    answer.append("8");
-                    num += 5;
-                    break;
-                default:
-                    answer.append("9");
-                    num += 4;
-                    break;
-            }
-        }
-        return Long.parseLong(answer.toString());
+        return Long.parseLong(
+                numbers.replace("zero", "0")
+                        .replace("one", "1")
+                        .replace("two", "2")
+                        .replace("three", "3")
+                        .replace("four", "4")
+                        .replace("five", "5")
+                        .replace("six", "6")
+                        .replace("seven", "7")
+                        .replace("eight", "8")
+                        .replace("nine", "9")
+        );
     }
 }
+
+// class Solution {
+//     public long solution(String numbers) {
+//         int num = 0;
+//         StringBuilder answer = new StringBuilder();
+        
+//         while(num < numbers.length()) {
+//             char ch = numbers.charAt(num);
+            
+//             switch (ch) {
+//                 case 'z':
+//                     answer.append("0");
+//                     num += 4;
+//                     break;
+//                 case 'o':
+//                     answer.append("1");
+//                     num += 3;
+//                     break;
+//                 case 't':
+//                     if (numbers.charAt(num + 1) == 'w') {
+//                         answer.append("2");
+//                         num += 3;
+//                     } else {
+//                         answer.append("3");
+//                         num += 5;
+//                     }
+//                     break;
+//                 case 'f':
+//                     if (numbers.charAt(num + 1) == 'o') {
+//                         answer.append("4");
+//                         num += 4;
+//                     } else {
+//                         answer.append("5");
+//                         num += 4;
+//                     }
+//                     break;
+//                 case 's':
+//                     if (numbers.charAt(num + 1) == 'i') {
+//                         answer.append("6");
+//                         num += 3;
+//                     } else {
+//                         answer.append("7");
+//                         num += 5;
+//                     }
+//                     break;
+//                 case 'e':
+//                     answer.append("8");
+//                     num += 5;
+//                     break;
+//                 default:
+//                     answer.append("9");
+//                     num += 4;
+//                     break;
+//             }
+//         }
+//         return Long.parseLong(answer.toString());
+//     }
+// }
