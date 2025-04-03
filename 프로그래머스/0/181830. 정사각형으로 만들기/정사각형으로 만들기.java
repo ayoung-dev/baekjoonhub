41d361869349a4019c890a47c3b368c589d7cb37
AYOUNG YANG <dkdudab@hanmail.net>
2025-04-03 23:56:16 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.11 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
@@ -1,28 +1,40 @@
-import java.util.Arrays;
+// import java.util.Arrays;
+
+// class Solution {
+//     public int[][] solution(int[][] arr) {
+//         int max = Math.max(arr.length, arr[0].length);
+//         int min = Math.min(arr.length, arr[0].length);
+        
+//         if (max == min) return arr;
+        
+//         int[][] answer = new int[max][max];
+        
+//         for (int i = 0; i < max; i++) {
+//             Arrays.fill(answer[i], 0);
+//         }
+        
+//         for (int i = 0; i < max; i++) {
+//             for (int j = 0; j < min; j++) {
+//                 if (max == arr.length)
+//                     answer[i][j] = arr[i][j];
+//                 else 
+//                     answer[j][i] = arr[j][i];
+//             }
+//         }
+        
+//         return answer;
+        
+//     }
+// }
 
 class Solution {
     public int[][] solution(int[][] arr) {
         int max = Math.max(arr.length, arr[0].length);
-        int min = Math.min(arr.length, arr[0].length);
-        
-        if (max == min) return arr;
-        
-        int[][] answer = new int[max][max];
-        
-        for (int i = 0; i < max; i++) {
-            Arrays.fill(answer[i], 0);
+        int[][] array = new int[max][max];
+
+        for (int i = 0; i < arr.length; i++) {
+            System.arraycopy(arr[i], 0, array[i], 0, arr[0].length);
         }
-        
-        for (int i = 0; i < max; i++) {
-            for (int j = 0; j < min; j++) {
-                if (max == arr.length)
-                    answer[i][j] = arr[i][j];
-                else 
-                    answer[j][i] = arr[j][i];
-            }
-        }
-        
-        return answer;
-        
+        return array;
     }
 }
