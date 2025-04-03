456d7cd9418d37ea72a6ec53266cb9bfd555a024
AYOUNG YANG <dkdudab@hanmail.net>
2025-04-03 23:55:10 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.67 ms, Memory: 90.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[][] solution(int[][] arr) {
+        int max = Math.max(arr.length, arr[0].length);
+        int min = Math.min(arr.length, arr[0].length);
+        
+        if (max == min) return arr;
+        
+        int[][] answer = new int[max][max];
+        
+        for (int i = 0; i < max; i++) {
+            Arrays.fill(answer[i], 0);
+        }
+        
+        for (int i = 0; i < max; i++) {
+            for (int j = 0; j < min; j++) {
+                if (max == arr.length)
+                    answer[i][j] = arr[i][j];
+                else 
+                    answer[j][i] = arr[j][i];
+            }
+        }
+        
+        return answer;
+        
+    }
+}
