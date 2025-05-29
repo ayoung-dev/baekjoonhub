ac916de8ab723ae94fd99d5aeae6fa2aa9115d51
AYOUNG YANG <dkdudab@hanmail.net>
2025-05-29 18:53:15 +0900

    [level 0] Title: 안전지대, Time: 0.04 ms, Memory: 87.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -0,0 +1,43 @@
+class Solution {
+    int length, answer;
+    int[][] temp;
+    
+    public int solution(int[][] board) {
+        length = board.length;
+        temp = new int[length+2][length+2];
+        answer = 0;
+        
+        for (int i = 0; i < length; i++) {
+            System.arraycopy(board[i], 0, temp[i + 1], 1, length);
+        }
+        
+        for (int i = 1; i < length + 1; i++) {
+            for (int j = 1; j < length + 1; j++) {
+                findDangerArea(i, j);
+            }
+        }
+        
+        countDangerArea(temp);
+        
+        return answer;
+    }
+    
+    void findDangerArea(int i, int j) {
+        if (temp[i][j] == 1) {
+            for (int k = i - 1; k <= i + 1; k++) {
+                for (int l = j - 1; l <= j + 1; l++)
+                    if (temp[k][l] != 1) {
+                        temp[k][l] = 2;
+                    }
+            }
+        }
+    }
+    
+    void countDangerArea(int[][] temp) {
+        for (int i = 1; i < length + 1; i++) {
+            for (int j = 1; j < length + 1; j++) {
+                if(temp[i][j] == 0) answer++;
+            }
+        }
+    }
+}
