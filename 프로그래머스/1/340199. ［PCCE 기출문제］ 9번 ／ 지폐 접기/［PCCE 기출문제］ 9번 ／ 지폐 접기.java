07d23b68ac46131fad90120fba15dc69bc510e51
AYOUNG YANG <dkdudab@hanmail.net>
2025-07-21 17:54:49 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.05 ms, Memory: 93 MB -BaekjoonHub

--- 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
@@ -1,24 +1,24 @@
 class Solution {
     public int solution(int[] wallet, int[] bill) {
         int answer = 0;
-        int w_min = Math.min(wallet[0],wallet[1]);
-        int w_max = Math.max(wallet[0],wallet[1]);
-        int b_min = Math.min(bill[0],bill[1]);
-        int b_max = Math.max(bill[0],bill[1]);
-        
-        while(true){
-            if(w_min < b_min || w_max < b_max){
-                b_max /= 2;
+        int wMax = Math.max(wallet[0], wallet[1]);
+        int wMin = Math.min(wallet[0], wallet[1]);
+        int bMax = Math.max(bill[0], bill[1]);
+        int bMin = Math.min(bill[0], bill[1]);
+
+        while(true) {
+            if(wMax < bMax || wMin < bMin){
+                bMax /= 2;
                 answer++;
             }
             
-            if(b_max < b_min){
-                int temp = b_max;
-                b_max = b_min;
-                b_min = temp;
+            if (bMin > bMax) {
+                int temp = bMax;
+                bMax = bMin;
+                bMin = temp;
             }
             
-            if(b_max <= w_max && b_min <= w_min){
+            if(bMax <= wMax && bMin <= wMin){
                 break;
             }
         }
