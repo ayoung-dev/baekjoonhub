9117fa409ae91e4dc1329a12f130660957c75e77
AYOUNG YANG <dkdudab@hanmail.net>
2025-07-22 20:39:56 +0900

    [level 1] Title: 소수 찾기, Time: 28.05 ms, Memory: 55.3 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -1,24 +1,24 @@
+import java.util.Arrays;
+
 class Solution {
-  public int solution(int n) {
-      int answer = 0;
-      boolean[] sosu =new boolean [n+1];
-      
-      for(int i=2; i<=n ; i++)
-          sosu[i]=true; //2~n번째수를 true로 초기화
-      
-      //제곱근 구하기    
-      int root=(int)Math.sqrt(n);
-      
-     for(int i=2; i<=root; i++){ //2~루트n까지 검사
-         if(sosu[i]==true){ //i번째의 수가 소수일 때
-             for(int j=i; i*j<=n; j++) //그 배수들을 다 false로 초기화(배수는 소수가 아니기 때문)
-                    sosu[i*j]=false;
-         }      
-     }
-      for(int i =2; i<=n; i++)  { 
-          if(sosu[i]==true) //소수의 개수 세기
-          answer++;
-      }
-      return answer;
-  }
+    public int solution(int n) {
+        int answer = 0;
+        boolean[] isPrime = new boolean[n + 1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        
+        for (int i = 0; i < isPrime.length; i++) {
+            if (isPrime[i]) answer++;
+        }
+        
+        return answer;
+    }
 }
