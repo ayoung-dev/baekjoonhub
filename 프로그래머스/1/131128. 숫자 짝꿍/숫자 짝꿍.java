bb2b8ad651c527203348b085cd3e7851e8f3eedd
AYOUNG YANG <dkdudab@hanmail.net>
2025-08-23 17:13:42 +0900

    [level 1] Title: 숫자 짝꿍, Time: 95.05 ms, Memory: 153 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -1,26 +1,27 @@
+import java.util.*;
+
 class Solution {
     public String solution(String X, String Y) {
-        StringBuilder answer = new StringBuilder();
-        int[] x = {0,0,0,0,0,0,0,0,0,0};
-        int[] y = {0,0,0,0,0,0,0,0,0,0};
-        for(int i=0; i<X.length();i++){
-           x[X.charAt(i)-48] += 1;
+        int[] countX = new int[10];
+        int[] countY = new int[10];
+        
+        for (char c : X.toCharArray()) {
+            countX[c - '0']++;
         }
-        for(int i=0; i<Y.length();i++){
-           y[Y.charAt(i)-48] += 1;
+        for (char c : Y.toCharArray()) {
+            countY[c - '0']++;
         }
-
-        for(int i=9; i >= 0; i--){
-            for(int j=0; j<Math.min(x[i],y[i]); j++){
-                answer.append(i);
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 9; i >= 0; i--) {
+            int common = Math.min(countX[i], countY[i]);
+            for (int j = 0; j < common; j++) {
+                sb.append(i);
             }
         }
-        if("".equals(answer.toString())){
-           return "-1";
-        }else if(answer.toString().charAt(0)==48){
-           return "0";
-        }else {
-            return answer.toString();
-        }
+        
+        if (sb.length() == 0) return "-1";  
+        if (sb.charAt(0) == '0') return "0";
+        return sb.toString();
     }
 }
