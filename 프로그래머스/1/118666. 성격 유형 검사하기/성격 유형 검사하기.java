6c8458f09f3d8f6d7118d44eec86a5cc33c5651a
AYOUNG YANG <dkdudab@hanmail.net>
2025-10-02 21:03:06 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 20.00 ms, Memory: 94.6 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -1,32 +1,45 @@
 import java.util.*;
+
 class Solution {
     public String solution(String[] survey, int[] choices) {
-        String answer = "";
-
-        int[] score = {0,3,2,1,0,1,2,3};
-        String[] user = {"R","T","C","F","J","M","A","N"};
-        int[] calc = {0,0,0,0,0,0,0,0}; 
-
-        int index = 0;
-        for(int i=0; i<survey.length; i++){
-            int target = choices[i]; 
-            String[] temp = {survey[i].substring(0,1), survey[i].substring(1,2)};
-            if( choices[i] < 4){ 
-                index = Arrays.asList(user).indexOf(temp[0]);
-                calc[index] += score[target];
-            }else {
-                
-                index = Arrays.asList(user).indexOf(temp[1]);
-                calc[index] += score[target];
+        Map<String, Integer> personal = new HashMap<>();
+        personal.put("R", 0);
+        personal.put("T", 0);
+        personal.put("C", 0);
+        personal.put("F", 0);
+        personal.put("J", 0);
+        personal.put("M", 0);
+        personal.put("A", 0);
+        personal.put("N", 0);
+        
+        for (int i = 0; i < survey.length; i++) {
+            if (choices[i] == 4) continue;
+            
+            String s = survey[i];
+            int point = Math.abs(choices[i] - 4);
+            
+            if (choices[i] < 4) {
+                s = survey[i].substring(0, 1);
+            } else {
+                s = survey[i].substring(1);
             }
+            personal.compute(s, (key, val) -> val + point);
         }
-        for (int i = 0; i < user.length; i += 2) {
-            if (calc[i] < calc[i + 1]) { 
-                answer += user[i + 1];
-            } else { 
-                answer += user[i];
-            }
-        }
-        return answer;
+        
+        String result = pick(personal, "R", "T")
+                      + pick(personal, "C", "F")
+                      + pick(personal, "J", "M")
+                      + pick(personal, "A", "N");
+        
+        return result;
+    }
+    
+    private static String pick(Map<String, Integer> map, String a, String b) {
+        int av = map.get(a), bv = map.get(b);
+        
+        if (av > bv) return a;
+        if (bv > av) return b;
+
+        return a.compareTo(b) < 0 ? a : b;
     }
 }
