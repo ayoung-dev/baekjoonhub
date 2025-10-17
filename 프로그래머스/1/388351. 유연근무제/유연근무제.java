0f58d95958fc79b85ffdab4d22ffee321b8a6d18
AYOUNG YANG <dkdudab@hanmail.net>
2025-10-17 21:17:23 +0900

    [level 1] Title: 유연근무제, Time: 0.67 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/유연근무제.java
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.java
@@ -5,8 +5,8 @@ class Solution {
         for (int i = 0; i < schedules.length; i++) {
             for (int j = 0; j < 7; j++) {
                 if ((startday + j) % 7 > 0 && (startday + j) % 7 < 6
-                   && 60 * (timelogs[i][j] / 100) + timelogs[i][j] % 100 > 60 * (schedules[i] / 100) 
-                    + schedules[i]%100+10) {
+                   && 60 * (timelogs[i][j] / 100) + timelogs[i][j] % 100 
+                    > 60 * (schedules[i] / 100) + schedules[i] % 100 + 10) {
                     answer--;
                     break;
                 }
