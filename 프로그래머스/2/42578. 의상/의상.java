e719f22f561211cffb4c1b4fac902f3cd8a88011
AYOUNG YANG <dkdudab@hanmail.net>
2025-10-22 22:34:10 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 80.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (String[] cloth: clothes) {
+            map.put(cloth[1], map.getOrDefault(cloth[1], 1) + 1);
+        }
+        
+        for (String str: map.keySet()) {
+            answer *= map.get(str);
+        }
+        
+        return answer -1;
+    }
+}
