20fae173b30367c08aec5d65a058aab0e04b0e81
AYOUNG YANG <dkdudab@hanmail.net>
2025-10-23 16:31:11 +0900

    [level 2] Title: H-Index, Time: 1.34 ms, Memory: 89.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        Arrays.sort(citations);
+        int len = citations.length;
+        int answer = 0;
+
+        for (int i = 0; i < len; i++) {
+            int h = len - i;
+            
+            if (citations[i] >= h) {
+                answer = h;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
