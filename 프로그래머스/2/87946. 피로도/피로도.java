be13e4fed848810a616e92a6692b4e3bb1b40d47
AYOUNG YANG <dkdudab@hanmail.net>
2025-10-25 14:19:38 +0900

    [level 2] Title: 피로도, Time: 3.00 ms, Memory: 88 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int answer = -1;
+    public boolean visited[];
+    
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+        
+        dfs(dungeons, k, 0);
+        
+        return answer;
+    }
+    
+    public void dfs(int[][] dungeons, int k, int depth) {
+        for (int i = 0; i < dungeons.length; i++) {
+            if (!visited[i] && dungeons[i][0] <= k) {
+                visited[i] = true;
+                dfs(dungeons, k - dungeons[i][1], depth + 1);
+                visited[i] = false;
+            }
+        }
+        answer = Math.max(answer, depth);
+    }
+}
