456a7b9b26859f6442325bc2d1ce6213e97842cb
AYOUNG YANG <dkdudab@hanmail.net>
2025-11-10 23:48:22 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 3.98 ms, Memory: 91.8 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -2,31 +2,49 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        HashMap<String, Integer> map = new HashMap<>();
+        List<Integer> answer = new ArrayList<>();
+        int len = privacies.length;
+        Map<String, Integer> termsMap = new HashMap<>();
+        int tYear = Integer.parseInt(today.substring(0, 4));
+        int tMonth = Integer.parseInt(today.substring(5, 7));
+        int tDate = Integer.parseInt(today.substring(8, 10));
         
-        int date = getDate(today);
-
-        for (String s : terms) {
-            String[] term = s.split(" ");
-            map.put(term[0], Integer.parseInt(term[1]));
+        for (String term: terms) {
+            String[] str = term.split(" ");
+            termsMap.put(str[0], Integer.parseInt(str[1]));
         }
-        
-        for (int i = 0; i < privacies.length; i++) {
-            String[] privacy = privacies[i].split(" ");
 
-            if (getDate(privacy[0]) + (map.get(privacy[1]) * 28) <= date) {
+        for (int i = 0; i < len; i++) {
+            String[] str = privacies[i].split(" ");
+            String dateStr = str[0];
+            String type = str[1];
+            int termMonth = termsMap.get(type);
+
+            int year = Integer.parseInt(dateStr.substring(0, 4));
+            int month = Integer.parseInt(dateStr.substring(5, 7));
+            int date = Integer.parseInt(dateStr.substring(8, 10));
+
+            month += termMonth;
+            year += (month - 1) / 12;
+            month = (month - 1) % 12 + 1;
+
+            date -= 1;
+            if (date == 0) {
+                date = 28;
+                month -= 1;
+                if (month == 0) {
+                    month = 12;
+                    year -= 1;
+                }
+            }
+
+            if (year < tYear ||
+                (year == tYear && month < tMonth) ||
+                (year == tYear && month == tMonth && date < tDate)) {
                 answer.add(i + 1);
             }
         }
-        return answer.stream().mapToInt(integer -> integer).toArray();
-    }
-
-    private int getDate(String today) {
-        String[] date = today.split("\\.");
-        int year = Integer.parseInt(date[0]);
-        int month = Integer.parseInt(date[1]);
-        int day = Integer.parseInt(date[2]);
-        return (year* 12* 28) + (month * 28) + day;
+        
+        return answer.stream().mapToInt(x -> x).toArray();
     }
 }
