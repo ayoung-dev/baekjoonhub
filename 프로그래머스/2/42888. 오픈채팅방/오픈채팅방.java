85e692b7d43e35bc14dba6052b9a8f0185571333
AYOUNG YANG <dkdudab@hanmail.net>
2026-02-01 14:31:14 +0900

    [level 2] Title: 오픈채팅방, Time: 225.49 ms, Memory: 151 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] record) {
+        List<String> answerList = new ArrayList<>();
+        Map<String, String> map = new HashMap<>(); 
+        
+        for (int i = 0; i < record.length; i++) {
+            String[] str = record[i].split(" ");
+            
+            if (!str[0].equals("Leave")) {
+                map.put(str[1], str[2]);
+            }
+        }
+
+        for (int i = 0; i < record.length; i++){
+            String[] str = record[i].split(" ");
+
+            if(str[0].equals("Enter")){
+                answerList.add(map.get(str[1]) + "님이 들어왔습니다.");
+            }
+            else if(str[0].equals("Leave")){
+                answerList.add(map.get(str[1]) + "님이 나갔습니다.");
+            }
+        }
+
+        return answerList.stream().toArray(String[]::new);
+    }
+}
