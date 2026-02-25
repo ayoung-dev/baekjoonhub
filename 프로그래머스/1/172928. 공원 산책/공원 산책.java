5cc17e1aff2b98f06fd0574eb6ffca45d2d8a903
AYOUNG YANG <dkdudab@hanmail.net>
2026-02-25 15:15:31 +0900

    [level 1] Title: 공원 산책, Time: 0.72 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -3,7 +3,7 @@ class Solution {
 
         int sRow = -1;
         int sCol = -1;
-
+        
         for (int i = 0; i < park.length; i++) {
             for (int j = 0; j < park[i].length(); j++) {
                 if (park[i].charAt(j) == 'S') {
@@ -18,37 +18,41 @@ class Solution {
             String[] r = route.split(" ");
             String op = r[0];
             int num = Integer.parseInt(r[1]);
-
+            
             int row = sRow;
             int col = sCol;
-
+            
             boolean canMove = true;
-
+            
             for (int k = 1; k <= num; k++) {
                 int nr = sRow;
                 int nc = sCol;
-
+                
                 switch (op) {
-                    case "N": nr = sRow - k; break;
-                    case "S": nr = sRow + k; break;
-                    case "W": nc = sCol - k; break;
-                    case "E": nc = sCol + k; break;
+                    case "N": nr = sRow - k;
+                        break;
+                    case "S": nr = sRow + k;
+                        break;
+                    case "W": nc = sCol - k;
+                        break;
+                    case "E": nc = sCol + k;
+                        break;
                 }
                 
                 if (nr < 0 || nr >= park.length || nc < 0 || nc >= park[0].length()) {
                     canMove = false;
                     break;
                 }
-
+                
                 if (park[nr].charAt(nc) == 'X') {
                     canMove = false;
                     break;
                 }
-
+                
                 row = nr;
                 col = nc;
             }
-
+            
             if (canMove) {
                 sRow = row;
                 sCol = col;
